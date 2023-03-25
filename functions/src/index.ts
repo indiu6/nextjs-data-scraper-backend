dd1bb2ba2d77667d70e685e76788b1c6af9abe8a
indiu6 <hewas6@gmail.com>
2023-03-25 18:51:43 -0400

    webhoob setup with ngrok

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from "firebase-functions";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const onScraperComplete = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
+
+// https://2ab4-2607-fea8-1e5f-81e0-7164-cb17-dd02-332e.ngrok.io/brightdata-scraper-15532/us-central1/onScraperComplete
