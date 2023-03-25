0b57623ffcbd1ca6baf39efe04794fdd616d296f
indiu6 <hewas6@gmail.com>
2023-03-25 18:57:33 -0400

    test webhook Noti from BrightData

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as functions from "firebase-functions";
 // https://firebase.google.com/docs/functions/typescript
 
 export const onScraperComplete = functions.https.onRequest((request, response) => {
+    console.log("SCRAPE COMPLETE >>> : ", request.body);
+
     functions.logger.info("Hello logs!", { structuredData: true });
     response.send("Hello from Firebase!");
 });
